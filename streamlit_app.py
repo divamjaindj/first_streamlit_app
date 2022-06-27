eae7ef9ce08a7bf40791042941e0f1d4a1d12a53
Divam <108296807+divamjaindj@users.noreply.github.com>
2022-06-28 00:07:27 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new healthier diner')
+
+streamlit.header('Menu deko')
+
+streamlit.text('Chole bhature')
+streamlit.text('Masala Dosa')
+streamlit.text('Omlette')
